af0c423bd1970c3c2d3dfba01e69d5696cc132d5
waqast <waqas@omnius.com>
2020-03-18 20:21:48 +0100

    update

--- bin/imageprocessing_blending.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3 
-import fire
-
-from imageprocessing import signature_blend
-
-fire.Fire(signature_blend)
--- bin/imageprocessing_image_tools.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3 
-import fire
-
-from imageprocessing import image_tools
-
-fire.Fire(image_tools)
